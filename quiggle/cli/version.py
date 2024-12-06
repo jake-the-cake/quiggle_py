83a07f2d2de9507bf4ac7cd73421506c8590c6e8
jake-the-cake <askjake331@gmail.com>
2024-12-06 13:45:06 -0500

    go to args

--- quiggle/cli/version.py
+++ quiggle/cli/version.py
@@ -6,7 +6,14 @@ class CliController:
     def __init__(self) -> None:
         if len(sys.argv) < 2: raise Exception('No command given.')
         self.args = sys.argv[1:]
+        self.command = self.args[0]
+        self.values = []
+        self.flags = []
+        self._parse_command()
 
+    def _parse_command(self) -> None:
+        if len(self.args) > 1:
+            print(self.args[1:])
 
 def update_version():
     cli = CliController()
