62818d6dd65610bdbdeb154e063768dbb144bbff
jake-the-cake <askjake331@gmail.com>
2024-12-07 14:54:27 -0500

    cli update version

--- quiggle/__main__.py
+++ quiggle/__main__.py
@@ -1,10 +1,11 @@
 ## local imports
 from quiggle.server import server
 from quiggle.cli.version import update_version
+from quiggle.config.globals import QUIGGLE_DIR
 
 def cli():
     print('cli')
-    update_version()
+    update_version(QUIGGLE_DIR + '/config/globals.py')
 
 if __name__ == '__main__':
     app = server.QuiggleServer(name = 'Angieland')
--- quiggle/cli/version.py
+++ quiggle/cli/version.py
@@ -4,17 +4,57 @@ import sys
 class CliController:
 
     def __init__(self) -> None:
-        if len(sys.argv) < 2: raise Exception('No command given.')
-        self.args = sys.argv[1:]
-        self.command = self.args[0]
-        self.values = []
-        self.flags = []
+        self.command:  str = ''
+        self.args:    list = []
+        self.values:  list = []
+        self.options: list = []
+        self.flags:   list = []
+        self._get_args()
         self._parse_command()
+        print(vars(self))
+
+    def _get_args(self):
+        if len(sys.argv) < 2:
+            return self._help_menu()
+        self.args:    list = sys.argv[1:]
 
     def _parse_command(self) -> None:
-        if len(self.args) > 1:
-            print(self.args[1:])
+        for arg in self.args:
+            if arg[0] != '-':
+                if self.command == '':
+                    self.command = arg
+                elif len(self.flags) == 0:
+                    self.values.append(arg)
+                else:
+                    self.flags[-1]['values'].append(arg)
+            elif arg[1] != '-':
+                self.options.append(arg[1:])
+            else:
+                self.flags.append({
+                    'flag': arg,
+                    'values': [] 
+                })
 
-def update_version():
+    def _help_menu(self):
+        print('Help Menu')
+
+    # def _ne
+
+def update_version(path: str) -> None:
     cli = CliController()
-    print(cli.args)
+    variable = 'VERSION_NUMBER'
+    with open(path) as file:
+        lines = file.readlines()
+    updated_lines = []
+    for line in lines:
+        print(line)
+        if line[:len(variable)] == variable:
+            split_line = line.replace(' ', '').replace('\n', '').split('=')
+            version_parts = split_line[1].replace('\'', '').split('.')
+            if len(cli.values) > 0:
+                split_line[1] = '\'' + cli.values[0] + '\'\n'
+            else:
+                split_line[1] = '.'.join(split_line[1])
+            updated_lines.append(' = '.join(split_line))
+        else: updated_lines.append(line)
+    print(updated_lines)
