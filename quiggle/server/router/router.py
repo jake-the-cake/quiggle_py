ab47559276cc05136120b250cdb208516bf19fb1
jake-the-cake <askjake331@gmail.com>
2024-12-17 12:49:39 -0500

    router init

--- quiggle/server/router/router.py
+++ quiggle/server/router/router.py
@@ -1,5 +1,29 @@
 ## local imports
+from quiggle.tools.reader.folder import FolderStructure
 
 class Router:
 
-	pass
+	def __init__(self):
+		self.routes: dict = {
+			'api': {
+				'static': [],
+				'dynamic': []
+			},
+			'html': {
+				'static': [],
+				'dynamic': []
+			}
+		}
+		self.tree: dict = self._set_tree()
+
+	def _set_tree(self):
+		pass
+
+	def check_route(self):
+		pass
+
+	def check_dynamic_route(self):
+		pass
+
+	# def check_route(self):
+	# 	pass
