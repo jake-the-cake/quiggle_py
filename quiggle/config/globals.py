ff159dfd669fcca034b667d5bb306d4d2ac8a129
jake-the-cake <askjake331@gmail.com>
2025-01-06 14:05:02 -0500

    rebuild color classes

--- quiggle/config/globals.py
+++ quiggle/config/globals.py
@@ -1,4 +1,4 @@
 from quiggle.config.root import get_quiggle_dir
 
-VERSION_NUMBER = '0.4.9'
+VERSION_NUMBER = '0.5.0'
 QUIGGLE_DIR = get_quiggle_dir()
--- quiggle/tools/logs/presets.py
+++ quiggle/tools/logs/presets.py
@@ -7,10 +7,55 @@ import shutil
 
 class ColorPrinter(Quiggle):
 
+	ON = '_on_'
+	WITH = '_with_'
+
 	def __init__(self, *message: tuple):
 		super().__init__()
-		self.message: list = self.toolkit.tuple_to_list(message)
+		self._colors:        str = Colors.RESET
+		self._message:      list = self.toolkit.tuple_to_list(message)
+		self._final_message: str = ''
 		self._timer = self._start_timer()
+
+	def line(self, scheme: str) -> None:
+		self._build_message(scheme)
+
+	def text(self, scheme: str) -> None:
+		self._build_message(scheme)
+
+	def _message_to_string(self) -> None:
+		self._final_message = ' '.join(self._message)
+
+	def _build_message(self, scheme: str = '', *callbacks: tuple[callable]) -> None:
+		self._cancel_timer()
+		if scheme != '': self._set_colors(scheme)
+		self._message_to_string()
+		self._reset_colors()
+		for callback in  callbacks:
+			callback()
+		self._print_message()
+
+	def _reset_colors(self) -> None:
+		self._final_message = self._final_message.replace(Colors.RESET, self._colors)
+
+	def _set_colors(self, scheme: str) -> None:
+		a = scheme.split(self.ON)
+		b = a[-1].split(self.WITH)
+		if len(a) == 1 and len(b) == 1:	b = []
+		colors = [a[0]] + b + ['', '']
+		self._add_colors(colors[:2])
+
+	def _add_colors(self, colors: list) -> None:
+		self._set_color(colors[0])
+		self._set_background(colors[1])
+
+	def _set_color(self, value: str) -> None:
+		value = value.upper()
+		if hasattr(Colors, value):
+			self._colors += Colors.get_attribute(value)
+
+	def _set_background(self, value: str) -> None:
+		self._set_color('BACKGROUND_' + value)
 
 	def _start_timer(self):
 		timer = self.toolkit.timer.callback_delay(0.1, self._print_message)
@@ -22,86 +67,77 @@ class ColorPrinter(Quiggle):
 			self._timer.cancel()
 
 	def _print_message(self) -> None:
-		print(self._color_code + self.message.__str__() + Colors.RESET)
+		print(self._colors + self._final_message.__str__() + Colors.RESET)
 
-	def _get_colors(self, format: str):
-		return getattr(Colors, format.upper())
+ColorPrinter('this', 'is', Colors.BLUE, 'a', Colors.RESET, 'message').text('red_on_white')
 
-	def format(self, format: str) -> None:
-		self._cancel_timer()
-		self._color_code = self._get_colors(format)
-		self._timer = self._start_timer()
-		# print(Colors.RED + self.message.__str__())
-
-ColorPrinter('this', 'is a message').format('red')
-
-def useColor(message: str, fg: str = None, bg: str = None):
+def useColor(message: str, foreground: str = None, background: str = None):
 	prefix: str = ''
-	if fg: prefix += Colors.get_attribute(fg) or ''
-	if bg: prefix += Colors.get_attribute('BACKGROUND_' + bg) or ''
+	if foreground: prefix += Colors.get_attribute(foreground) or ''
+	if background: prefix += Colors.get_attribute('BACKGROUND_' + background) or ''
 	return prefix + message + Colors.RESET
 
 class UseColor:
 
 	@staticmethod
 	def red(*content) -> str:
-		return useColor(*content, fg='red')
+		return useColor(*content, foreground='red')
 	
 	@staticmethod
 	def yellow(*content) -> str:
-		return useColor(*content, fg='yellow')
+		return useColor(*content, foreground='yellow')
 	
 	@staticmethod
 	def brightgreen(*content) -> str:
-		return useColor(*content, fg='bright_green')
+		return useColor(*content, foreground='bright_green')
 
 	@staticmethod
 	def white_on_red(*content) -> str:
-		return useColor(*content, fg='white', bg='red')		
+		return useColor(*content, foreground='white', background='red')		
 	
 	@staticmethod
 	def white_on_green(*content) -> str:
-		return useColor(*content, fg='white', bg='green')		
+		return useColor(*content, foreground='white', background='green')		
 	
 	@staticmethod
 	def black_on_yellow(*content) -> str:
-		return useColor(*content, fg='black', bg='yellow')		
+		return useColor(*content, foreground='black', background='yellow')		
 	
 	@staticmethod
 	def black_on_lightgray(*content) -> str:
-		return useColor(*content, fg='black', bg='light_gray')
+		return useColor(*content, foreground='black', background='light_gray')
 	
 	@staticmethod
 	def black_on_magenta(*content) -> str:
-		return useColor(*content, fg='black', bg='magenta')
+		return useColor(*content, foreground='black', background='magenta')
 
 	@staticmethod
 	def white_on_magenta(*content) -> str:
-		return useColor(*content, fg='white', bg='magenta')
+		return useColor(*content, foreground='white', background='magenta')
 
 	@staticmethod
-	def fg_yellow(*content) -> str:
-		return useColor(*content, fg='yellow')[:-4]	
+	def foreground_yellow(*content) -> str:
+		return useColor(*content, foreground='yellow')[:-4]	
 	
 	@staticmethod
-	def fg_red(*content) -> str:
-		return useColor(*content, fg='red')[:-4]	
+	def foreground_red(*content) -> str:
+		return useColor(*content, foreground='red')[:-4]	
 	
 	@staticmethod
-	def fg_white(*content) -> str:
-		return useColor(*content, fg='white')[:-4]	
+	def foreground_white(*content) -> str:
+		return useColor(*content, foreground='white')[:-4]	
 	
 	@staticmethod
-	def fg_blue(*content) -> str:
-		return useColor(*content, fg='blue')[:-4]	
+	def foreground_blue(*content) -> str:
+		return useColor(*content, foreground='blue')[:-4]	
 	
 	@staticmethod
-	def fg_brightgreen(*content) -> str:
-		return useColor(*content, fg='bright_green')[:-4]
+	def foreground_brightgreen(*content) -> str:
+		return useColor(*content, foreground='bright_green')[:-4]
 
 	@staticmethod
-	def fg_black(*content) -> str:
-		return useColor(*content, fg='black')[:-4]
+	def foreground_black(*content) -> str:
+		return useColor(*content, foreground='black')[:-4]
 class Print:
 
 	@staticmethod
@@ -130,11 +166,11 @@ class Print:
 	
 	@staticmethod
 	def blue(*content) -> None:
-		print(useColor(*content, fg='blue'))
+		print(useColor(*content, foreground='blue'))
 	
 	@staticmethod
 	def cyan(*content) -> None:
-		print(useColor(*content, fg='cyan'))
+		print(useColor(*content, foreground='cyan'))
 
 	@staticmethod
 	def black_on_magenta(*content) -> None:
@@ -178,16 +214,16 @@ class Printline(Print):
 
 
 def infolog(message: str) -> str:
-	return useColor(message, fg='cyan')
+	return useColor(message, foreground='cyan')
 
 def labellog(message: str) -> str:
-	return useColor(message, fg='yellow')
+	return useColor(message, foreground='yellow')
 
 def questionlog(message: str) -> str:
-	return useColor(message, fg='bright-green')
+	return useColor(message, foreground='bright-green')
 
 def errorlog(message: str) -> str:
-	return useColor(f' { message } ', fg='white', bg='red')
+	return useColor(f' { message } ', foreground='white', background='red')
 
 def buglog(message: str) -> None:
 	return (
